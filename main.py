f7135334e5efcbfb2e18042d63ebbaaba84e929e
ernesthenning <vladmalff@gmail.com>
2025-01-20 17:20:42 +0300

    рефакторинг

--- main.py
+++ main.py
@@ -45,21 +45,21 @@ def generate():
     #for parameter in parameters:
      #   print(parameter)
 
-    gen = Generator(length_points=12001,
+    gen = Generator(length_points=15001,
                     dots_per_minute=600,
                     bias_left_border=1000,
-                    bias_right_border=5200,
-                    bias_max_intensity=100,
-                    bias_min_intensity=1,
-                    bias_peaks_number=50,
-                    bias_max_peak_width=300,
-                    bias_min_peak_width=30,
-                    peaks_probe_number=2,
-                    widths_probe=np.array([[125, 100], [125, 100], [125, 100], [125, 100], [125, 100], [125, 100], [125, 100], [125, 100]]),
-                    intensities_probe_peaks=np.array([[242, 795.8], [242, 795.8], [242, 795.8], [242, 795.8], [242, 795.8], [242, 795.8], [242, 795.8], [242, 795.8]]),
-                    times_probe_peaks=np.array([[6300, 7100], [6300, 7100], [6300, 7100], [6300, 7100], [6300, 7100], [6300, 7100], [6300, 7100], [6300, 7100]]),
-                    repeatability=0.03,
-                    reproducibility=0.05,
+                    bias_right_border=3900,
+                    bias_max_intensity=15,
+                    bias_min_intensity=0.5,
+                    bias_peaks_number=12,
+                    bias_max_peak_width=50,
+                    bias_min_peak_width=20,
+                    peaks_probe_number=3,
+                    widths_probe=np.array([[120, 150, 110], [120, 150, 110], [120, 150, 110], [120, 150, 110], [120, 150, 110], [120, 150, 110], [120, 150, 110], [120, 150, 110]]),
+                    intensities_probe_peaks=np.array([[147, 98, 121], [147, 98, 121], [147, 98, 121], [147, 98, 121], [147, 98, 121], [147, 98, 121], [147, 98, 121], [147, 98, 121]]),
+                    times_probe_peaks=np.array([[5700, 6600, 12000], [5700, 6600, 12000], [5700, 6600, 12000], [5700, 6600, 12000], [5700, 6600, 12000], [5700, 6600, 12000], [5700, 6600, 12000], [5700, 6600, 12000]]),
+                    repeatability=0.02,
+                    reproducibility=0.04,
                     dead_time=1200,
                     dead_width=100,
                     dead_intensity=2,
@@ -68,14 +68,14 @@ def generate():
                     number_chromatograms_in_series=2,
                     noise_intensity=0.3,
                     probes_count=8,
-                    start_time='18:58',
-                    interval_between_chromatograms=22,
+                    start_time='11:41',
+                    interval_between_chromatograms=26,
                     cyctime=0.1,
                     coef=0.0003,
                     units='mV',
-                    analysis_date='17.01.2025')
+                    analysis_date='20.01.2025')
     gen.get_exported_chromatograms()
-    gen.generate_bats("17.01.2025")
+    gen.generate_bats("20.01.2025")
 
 
 if __name__ == '__main__':
